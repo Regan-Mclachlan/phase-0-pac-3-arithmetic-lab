8ef2dccd9a221c697708dab77138ff81c79b180a
Regan-Mclachlan <reganmc96@hotmail.com>
2021-03-01 21:02:03 +1000

    arithmetic calculator

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+let add = function(a, b) {return (a + b)}
+
+let subtract = function (a,b) {return (a - b)};
+
+let multiply = function(a, b) {return (a * b)};
+
+let divide = function(a, b) {return (a / b)};
+
+let increment = function(n){
+return n + 1;
+};
+
+let decrement = function(n){
+return n - 1;
+};
+
+function makeInt(n){
+return parseInt(n, 10);
+};
+  
+let assume = function(n){
+return n = 10
+};
+
+function preserveDecimal(n){
+return parseInt(n)
+};
+
+function preserveDecimal(n){ return parseFloat(n)};
